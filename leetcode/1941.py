3ce988dfe6af4afc03a43c2bdc283eb45da83526
alibayomy <alibayomy12@gmail.com>
2025-09-20 15:33:53 +0300

    Better sol for 1941

--- leetcode/1941.py
+++ leetcode/1941.py
@@ -1,16 +1,17 @@
 """1941. Check if All Characters Have Equal Number of Occurrences"""
-
+import collections
 class Solution:
+    # def areOccurrencesEqual(self, s: str) -> bool:
+    #     frequency = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    #     last = 0
+    #     for letter in s:
+    #         current = frequency[ord(letter) - ord('a')]
+    #         frequency[ord(letter) - ord('a')] = current + 1
+    #         last = current + 1
+    #     for f in frequency:
+    #         if f != 0:
+    #             if f != last:
+    #                 return False
+    #     return True
     def areOccurrencesEqual(self, s: str) -> bool:
-        frequency = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        last = 0
-        for letter in s:
-            current = frequency[ord(letter) - ord('a')]
-            frequency[ord(letter) - ord('a')] = current + 1
-            last = current + 1
-        for f in frequency:
-            if f != 0:
-                if f != last:
-                    return False
-        return True
-            
+        return len(collections.Counter(collections.Counter(s).values())) == 1
